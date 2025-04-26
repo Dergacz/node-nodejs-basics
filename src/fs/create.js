1fa0e91432fd840fcaabef8c4042dedbc0ec230c
Stanislau <sats96dergach@gmail.com>
2025-04-26 12:26:41 +0200

    init: start node js basic

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,20 @@
+import fs from 'fs/promises';
+import { join } from 'path';
+
 const create = async () => {
-    // Write your code here 
+  const content = 'I am fresh and young';
+  const errorMessage = 'FS operation failed must be thrown';
+  const filePath = join('./files, fresh.txt');
+
+  try {
+    await fs.access(filePath);
+    throw new Error(errorMessage);
+  } catch (error) {
+    if (error.message === errorMessage) {
+      throw error;
+    }
+    await fs.writeFile(filePath, content, 'utf8');
+  }
 };
 
 await create();
