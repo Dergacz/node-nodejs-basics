5041ac32ce9fec1781b38438a1c6f988ad07648a
Stanislau <sats96dergach@gmail.com>
2025-04-27 13:21:31 +0200

    feat: add cli tasks

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,15 @@
 const parseArgs = () => {
-    // Write your code here 
+  const args = process.argv.slice(2);
+  const arr = [];
+
+  for (let i = 0; i < args.length; i += 2) {
+    const propName = args[i].replace('--', '');
+    const value = args[i + 1];
+    arr.push(`${propName} is ${value}`);
+  }
+  const result = arr.join(', ');
+
+  console.log(result);
 };
 
 parseArgs();
