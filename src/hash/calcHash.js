4387323786145b3ed820e7d38c7213cd77826bfc
Stanislau <sats96dergach@gmail.com>
2025-04-28 20:45:50 +0200

    feat: add calc hash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,26 @@
+import { createReadStream } from 'node:fs';
+import { createHash } from 'crypto';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
 const calculateHash = async () => {
-    // Write your code here 
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = path.dirname(__filename);
+  const filePath = path.join(__dirname, 'files', 'fileToCalculateHashFor.txt');
+  const hash = createHash('sha256');
+  const stream = createReadStream(filePath);
+
+  stream.on('data', (chunk) => {
+    hash.update(chunk);
+  });
+
+  stream.on('end', () => {
+    console.log(hash.digest('hex'));
+  });
+
+  stream.on('error', (err) => {
+    console.error('Error reading the file:', err.message);
+  });
 };
 
 await calculateHash();
