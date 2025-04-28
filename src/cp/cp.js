5b2cf78b97eccd3bf6cf6c8fef33edbc3686a694
Stanislau <sats96dergach@gmail.com>
2025-04-28 22:12:18 +0200

    feat: add wt

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,14 @@
+import { spawn }from 'child_process';
+import path from 'path';
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+    const scriptPath = path.join(import.meta.dirname, 'files', 'script.js');
+    const child = spawn('node', [scriptPath, ...args], {
+        stdio: ['pipe', 'pipe', 'inherit']
+    });
+
+    process.stdin.pipe(child.stdin);
+    child.stdout.pipe(process.stdout);
 };
 
-// Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess( ['arg1', 'arg2', 'arg3']);
--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,37 @@
+import { Worker } from 'worker_threads';
+import os from 'os';
+import path from 'path';
+
 const performCalculations = async () => {
-    // Write your code here
+  const numCores = os.cpus().length;
+  const results = [];
+  const workers = [];
+  const promises = [];
+
+  for (let i = 0; i < numCores; i++) {
+    const worker = new Worker(path.resolve(import.meta.dirname, 'worker.js'));
+    workers.push(worker);
+    promises.push(new Promise((resolve) => {
+      worker.once('message', (msg) => {
+        resolve(msg);
+      });
+      worker.once('error', () => {
+        resolve({status: 'error', data: null});
+      });
+      worker.once('exit', (code) => {
+        if (code !== 0) {
+          resolve({status: 'error', data: null});
+        }
+      });
+    }));
+    worker.postMessage(10 + i);
+  }
+
+  const settledResults = await Promise.all(promises);
+  for (const result of settledResults) {
+    results.push(result);
+  }
+  console.log(results);
 };
 
 await performCalculations();
